45a7b5d1b5df71b1ef00f3a518c79f745b812610
charmainpdrgl <charmainpedrigal@gmail.com>
2021-03-12 21:57:00 +1300

    added code

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,3 +85,19 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+// Replacing '/' would result in empty string which is invalid
+const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+  const oldPage = Object.assign({}, page)
+  // Remove trailing slash unless page is /
+  page.path = replacePath(page.path)
+  if (page.path !== oldPage.path) {
+    // Replace old page with new page
+    deletePage(oldPage)
+    createPage(page)
+  }
+}
